2cd7ff1f4b57f43acf853eb682459224f2aa16cc
V.Prochniy <procnyjvladimir@gmail.com>
2022-09-09 20:39:14 +0300

    test_3

--- js/myscript.js
+++ js/myscript.js
@@ -1,15 +1,34 @@
-alert(5 % 2);
-alert(1 + 2 + '1');
+let n = 5;
+n *= 2 + 1;
+console.log(n);
+n -= 3;
+console.log(n);
 
-let apples = '4';
-let oranges = '3';
-alert(apples - oranges);
+n++;
+console.log(n);
 
-alert(+apples - +oranges);
+let counter = 1;
+let a = ++counter; // (*)
 
-let x = 2 * 2 + 1;
-alert(x);
-let b = 2;
-let c = 3 - (x = b + -1);
-alert(x);
-alert(c);
+console.log(a); //2
+
+let counters = 1;
+let b = counters++;
+console.log(b); //1
+let h = (1 + 2, 2 + 3);
+console.log(h);
+
+"" + 1 + 0 = "10" // (1)
+"" - 1 + 0 = -1 // (2)
+true + false = 1
+6 / "3" = 2
+"2" * "3" = 6
+4 + 5 + "px" = "9px"
+"$" + 4 + 5 = "$45"
+"4" - 2 = 2
+"4px" - 2 = NaN
+"  -9  " + 5 = "  -9  5" // (3)
+"  -9  " - 5 = -14 // (4)
+null + 1 = 1 // (5)
+undefined + 1 = NaN // (6)
+" \t \n" - 2 = -2 // (7)

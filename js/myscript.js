dc369380b377fd37a08b1afd43c0e7b50f344542
V.Prochniy <procnyjvladimir@gmail.com>
2022-09-09 09:34:23 +0300

    test_3

--- js/myscript.js
+++ js/myscript.js
@@ -1,27 +1,15 @@
-let numberOfFilms = ('Сколько фильмов вы уже посмотрели?', '');
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
-
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies.a = b;
-personalMovieDB.movies.c = d;
-
-console.log(personalMovieDB);
-if (4 == 4);
-console.log('ok');
-
 alert(5 % 2);
 alert(1 + 2 + '1');
 
-let apples = '2';
+let apples = '4';
 let oranges = '3';
-alert('2' + '3');
+alert(apples - oranges);
+
+alert(+apples - +oranges);
+
+let x = 2 * 2 + 1;
+alert(x);
+let b = 2;
+let c = 3 - (x = b + -1);
+alert(x);
+alert(c);

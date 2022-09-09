ef5a67ab581932b5c0323c9957745871899d13af
V.Prochniy <procnyjvladimir@gmail.com>
2022-09-09 08:03:23 +0300

    test_2

--- js/myscript.js
+++ js/myscript.js
@@ -12,7 +12,16 @@ const b = prompt('На сколько оцените его?', '');
 const c = prompt('Один из последних просмотренных фильмов?', '');
 const d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+personalMovieDB.movies.a = b;
+personalMovieDB.movies.c = d;
 
 console.log(personalMovieDB);
+if (4 == 4);
+console.log('ok');
+
+alert(5 % 2);
+alert(1 + 2 + '1');
+
+let apples = '2';
+let oranges = '3';
+alert('2' + '3');

fbd7444c4e7c77053f4d07750a745ab366b89dfe
V.Prochniy <procnyjvladimir@gmail.com>
2022-09-12 22:47:04 +0300

    imput_commit

--- js/myscript.js
+++ js/myscript.js
@@ -1,34 +1,13 @@
-let n = 5;
-n *= 2 + 1;
-console.log(n);
-n -= 3;
-console.log(n);
-
-n++;
-console.log(n);
-
-let counter = 1;
-let a = ++counter; // (*)
-
-console.log(a); //2
-
-let counters = 1;
-let b = counters++;
-console.log(b); //1
-let h = (1 + 2, 2 + 3);
-console.log(h);
-
-"" + 1 + 0 = "10" // (1)
-"" - 1 + 0 = -1 // (2)
-true + false = 1
-6 / "3" = 2
-"2" * "3" = 6
-4 + 5 + "px" = "9px"
-"$" + 4 + 5 = "$45"
-"4" - 2 = 2
-"4px" - 2 = NaN
-"  -9  " + 5 = "  -9  5" // (3)
-"  -9  " - 5 = -14 // (4)
-null + 1 = 1 // (5)
-undefined + 1 = NaN // (6)
-" \t \n" - 2 = -2 // (7)
+const button = document.querySelector('button');
+let input = document.querySelector('.age');
+button.onclick = () => {
+  // => соответсвует function(анонимная финкция)
+  let num = +input.value;
+  if (num >= 16 && num < 60) {
+    alert('welcom!!');
+  } else if (num >= 60) {
+    alert('yu fak!!');
+  } else {
+    alert('No!!');
+  }
+};
